9b542f02f27874d44f55541e7397bd424237fd82
danny-andersen <danny_andersen@yahoo.com>
2019-01-14 21:46:07 +0000

    Added show current schedule button

--- lib/schedule-tab.dart
+++ lib/schedule-tab.dart
@@ -13,7 +13,8 @@ class SchedulePage extends StatefulWidget {
   final HttpClient client;
 
   @override
-  State createState() => SchedulePageState(client: this.client, oauthToken: this.oauthToken);
+  State createState() =>
+      SchedulePageState(client: this.client, oauthToken: this.oauthToken);
 }
 
 class SchedulePageState extends State<SchedulePage> {
@@ -22,6 +23,8 @@ class SchedulePageState extends State<SchedulePage> {
   final String oauthToken;
   final HttpClient client;
   final String scheduleFilesPattern = "setSchedule.txt.";
+  final String currentScheduleFile = "setSchedule.txt.current";
+  String currentSchedulePath;
 
   List<Schedule> schedules;
   HttpClient httpClient = new HttpClient();
@@ -31,11 +34,15 @@ class SchedulePageState extends State<SchedulePage> {
   List<DropdownMenuItem<String>> scheduleDays;
   String selectedDayRange;
   List<charts.Series> hourTempSeries;
+  bool showNowEnabled = false;
+  bool showNowSchedulePressed = false;
 
   @override
   void initState() {
     getSchedules();
-    hourTempSeries = PointsLineChart.createSeries([TempByHour(0, 10.0), TempByHour(2400, 10.0)]);
+    showNowEnabled = false;
+    hourTempSeries = PointsLineChart.createSeries(
+        [TempByHour(0, 10.0), TempByHour(2400, 10.0)]);
     super.initState();
   }
 
@@ -50,16 +57,21 @@ class SchedulePageState extends State<SchedulePage> {
   void processScheduleFiles(FileListing files) {
     //Process each file and add to dropdown
     this.scheduleEntries = List();
-    for (FileEntry file in files.fileEntries) {
+    setState(() {
+      for (FileEntry file in files.fileEntries) {
 //      print("Adding ${file.fileName}");
-      ScheduleEntry schedule = ScheduleEntry.fromFileEntry(file);
-      setState(() {
+        ScheduleEntry schedule = ScheduleEntry.fromFileEntry(file);
         this.scheduleEntries.add(DropdownMenuItem<ScheduleEntry>(
-          value: schedule,
-          child: new Text(schedule.name),
-        ));
-      });
-    }
+              value: schedule,
+              child: new Text(schedule.name),
+            ));
+        if (file.fileName.compareTo(currentScheduleFile) == 0) {
+          this.currentSchedulePath = file.fullPathName;
+          this.selectedScheduleEntry = schedule;
+        }
+      }
+      showNowEnabled = true;
+    });
   }
 
   void scheduleSelected(ScheduleEntry scheduleEntry) {
@@ -91,6 +103,10 @@ class SchedulePageState extends State<SchedulePage> {
               child: new Text(dayRange),
             ));
     });
+    if (showNowSchedulePressed) {
+      daySelected(ScheduleDay.weekDaysByInt[DateTime.now().weekday]);
+      showNowSchedulePressed = false;
+    }
   }
 
   void daySelected(String day) {
@@ -100,18 +116,45 @@ class SchedulePageState extends State<SchedulePage> {
     });
   }
 
-
   //Process schedule for selected day to create Series to plot
   void generateHourTempSeries(String day) {
-    List<ScheduleDay> dayEntries = this.selectedSchedule.filterEntriesByDayRange(day);
-    List<TempByHour> tempPoints = Schedule.generateTempByHourForEntries(dayEntries);
+    List<ScheduleDay> dayEntries =
+        this.selectedSchedule.filterEntriesByDayRange(day);
+    List<TempByHour> tempPoints =
+        Schedule.generateTempByHourForEntries(dayEntries);
 //    tempPoints.forEach((th) => print("Time: ${th.hour} Temp: ${th.temperature}"));
     hourTempSeries = PointsLineChart.createSeries(tempPoints);
+  }
+
+  void showNowSchedule() {
+    showNowSchedulePressed = true;
+    DropBoxAPIFn.getDropBoxFile(
+      client: this.httpClient,
+      oauthToken: this.oauthToken,
+      fileToDownload: currentSchedulePath,
+      callback: processScheduleFile,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     Widget returnWidget = ListView(children: [
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: RaisedButton(
+              child: Text('Show current schedule'),
+              elevation: 5,
+              onPressed: showNowEnabled ? showNowSchedule : null,
+//              color: Colors.blue,
+            ),
+          )
+        ],
+      ),
       Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.start,
@@ -120,8 +163,8 @@ class SchedulePageState extends State<SchedulePage> {
             Container(
               padding: const EdgeInsets.only(top: 10.0, bottom: 16.0),
               child: Text(
-                'Select Saved Schedule to view/modify:',
-                style: Theme.of(context).textTheme.title,
+                'Or select a schedule to view/modify:',
+                style: Theme.of(context).textTheme.body1,
               ),
             ),
             Container(
@@ -146,7 +189,7 @@ class SchedulePageState extends State<SchedulePage> {
               padding: const EdgeInsets.only(top: 10.0, bottom: 16.0),
               child: Text(
                 'Select Day Range to show:',
-                style: Theme.of(context).textTheme.title,
+                style: Theme.of(context).textTheme.body1,
               ),
             ),
             Container(
@@ -218,6 +261,12 @@ class PointsLineChart extends StatelessWidget {
   charts.RangeAnnotation getRangeAnnotation() {
     double minValue = getMinValue();
     List<charts.LineAnnotationSegment> lines = List();
+    int nowTime = TempByHour.from(DateTime.now(),0.0).hour;
+    lines.add(new charts.LineAnnotationSegment(
+        nowTime, charts.RangeAnnotationAxisType.domain,
+        strokeWidthPx: 30,
+//        color: charts.Color.fromHex('#ff0000');,
+        startLabel: domainFormat.format(nowTime)));
     for (charts.Series s in this.seriesList)
       for (TempByHour point in s.data)
         if (point.temperature != minValue)
@@ -249,8 +298,6 @@ class PointsLineChart extends StatelessWidget {
       ),
       defaultRenderer: new charts.LineRendererConfig(includePoints: true),
       behaviors: [
-        new charts.PanAndZoomBehavior(),
-        new charts.SlidingViewport(),
         getRangeAnnotation(),
       ],
     );
--- lib/schedule.dart
+++ lib/schedule.dart
@@ -72,6 +72,16 @@ class ScheduleDay {
     'Fri',
     'Sat'
   ];
+
+  static final Map<int, String> weekDaysByInt = {
+  7: 'Sun',
+  1: 'Mon',
+  2:'Tues',
+  3: 'Wed',
+  4: 'Thurs',
+  5: 'Fri',
+  6: 'Sat',
+  };
 }
 
 //Schedule is a simple list of ScheduleDay entries
